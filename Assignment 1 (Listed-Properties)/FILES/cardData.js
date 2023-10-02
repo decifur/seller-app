70ff0b17a1a5bd2bece2f10484b6249dc217c7ba
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-10-02 15:13:58 +0530

    files

---
+++ Assignment 1 (Listed-Properties)/FILES/cardData.js
@@ -0,0 +1,88 @@
+import Card from "../../components/Content/Card";
+
+const propData = [
+    {
+      id: 1,
+      name: "RCE Theaters - 907 S Beckford Dr, Henderson, NC 27536",
+      price: "$7,255",
+      location: "8558 Pecan St.",
+      image: "/images/city1.jpg",
+      type: "For Rent",
+      popular: false,
+    },
+    {
+      id: 2,
+      name: "Costco Wholesale - 1021 Oak Forest Ln, Myrtle Beach, SC",
+      price: "$7,255",
+      location: "8558 Parker Rd.",
+      image: "/images/city2.jpg",
+      type: "For Rent",
+      popular: true,
+    },
+    {
+      id: 3,
+      name: "Regal North Hills - 4150 Main at North Hills St, Releigh",
+      price: "$5,256",
+      location: "3890 Poplar Dr.",
+      image: "/images/city3.jpg",
+      type: "For Sale",
+      popular: true,
+    },
+    {
+      id: 4,
+      name: "Spring Lane Cinemas - 1351 Plaza Blvd, Sanford, NC 27330",
+      price: "$7,255",
+      location: "8558 Green Rd.",
+      image: "/images/city4.jpg",
+      type: "For Rent",
+      popular: false,
+    },
+    {
+      id: 5,
+      name: "RCE Theaters - 907 S Beckford Dr, Henderson, NC 27536",
+      price: "$7,255",
+      location: "8558 Green Rd.",
+      image: "/images/city5.jpg",
+      type: "For Sale",
+      popular: true,
+    },
+    {
+      id: 6,
+      name: "Dollar General - 5416 Rock Quarry Rd, Raleigh, NC 27610",
+      price: "$7,255",
+      location: "8558 Parker Rd.",
+      image: "/images/city6.jpg",
+      type: "For Rent",
+      popular: false,
+    },
+
+    {
+      id: 7,
+      name: "RCE Theaters - 907 S Beckford Dr, Henderson, NC 27536",
+      price: "$10,255",
+      location: "8558 Pecan St.",
+      image: "/images/city7.jpg",
+      type: "For Rent",
+      popular: false,
+    },
+    {
+      id: 8,
+      name: "Costco Wholesale - 1021 Oak Forest Ln, Myrtle Beach, SC",
+      price: "$17,255",
+      location: "8558 Parker Rd.",
+      image: "/images/city8.jpg",
+      type: "For Rent",
+      popular: true,
+    },
+    {
+      id: 9,
+      name: "Regal North Hills - 4150 Main at North Hills St, Releigh",
+      price: "$15,256",
+      location: "3890 Poplar Dr.",
+      image: "/images/city9.jpg",
+      type: "For Sale",
+      popular: true,
+    },
+  ];
+
+  export default propData;
